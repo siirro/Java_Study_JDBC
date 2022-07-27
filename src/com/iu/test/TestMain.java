8c2074634c8faaab665f2861b9b18a80f14fc36c
goodeecomputer <viostarryy@gmail.com>
2022-07-27 12:20:00 +0900

    test end

--- src/com/iu/test/TestMain.java
+++ src/com/iu/test/TestMain.java
@@ -1,9 +1,17 @@
 package com.iu.test;
 
+import com.iu.util.DBConnector;
+
 public class TestMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		DBConnector dbConnector = new DBConnector();
+		try {
+			dbConnector.getConnection();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
